c93a94636a2f4e75800e1eaaa50373e4d14d750e
cudderson <codered1227@gmail.com>
2021-06-27 16:53:39 -0500

    playing with canvas, create moving rect on button click

--- simulator.js
+++ simulator.js
@@ -1,10 +1,25 @@
-document.addEventListener("DOMContentLoaded", f);
+document.addEventListener("DOMContentLoaded", setup);
 
-function f () {
-    var message = 'hello, world! This is a javascript string.';
-    var placeholder = document.getElementById("test");
+function setup () {
 
-    placeholder.innerHTML = message;
+    // get canvas element
+    var canvas = document.getElementById("main-canvas");
+    
+    // get drawing object
+    var ctx = canvas.getContext("2d");
+
+    // create rect object
+    ctx.fillStyle = "lightblue";
+    ctx.fillRect(25, 250, 25, 25);
 
     console.log("Javascript working.");
+
+    var btn = document.getElementById("btn");
+    console.log(btn.innerHTML);
+
+    btn.addEventListener('click', function () {
+        ctx.clearRect(25, 250, 25, 25);
+        ctx.translate(50, 50);
+        ctx.fillRect(25, 250, 25, 25);
+    });
 }
