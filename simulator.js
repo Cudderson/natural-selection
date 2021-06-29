41c8bec9692c4d73e05291773983ac0f90a63270
cudderson <codered1227@gmail.com>
2021-06-29 13:46:05 -0500

    create rough Organism class

--- simulator.js
+++ simulator.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", setup);
 
-function setup () {
-
-    function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1) + min); // min and max inclusive
+class Organism {
+    constructor (x, y, ctx) {
+        this.x = x;
+        this.y = y;
+        this.ctx = ctx;
     }
+}
+
+function setup () {
 
     // get canvas element
     var canvas = document.getElementById("main-canvas");
@@ -18,15 +20,26 @@ function setup () {
     ctx.fillStyle = "lightblue";
     ctx.fillRect(300, 300, 10, 10);
 
-    console.log("Javascript working.");
+    setInterval(moveOrganism, 100);
+    console.log("SETUP COMPLETE");
+}
 
-    setInterval(moveSquare, 100);
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // min and max inclusive
+}
 
-    function moveSquare () {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        var x = getRandomInt(-10, 10);
-        var y = getRandomInt(-10, 10);
-        ctx.translate(x, y);
-        ctx.fillRect(300, 300, 10, 10);
-    } 
+function moveOrganism () {
+    // get canvas element
+    var canvas = document.getElementById("main-canvas");
+    
+    // get drawing object
+    var ctx = canvas.getContext("2d");
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    var x = getRandomInt(-10, 10);
+    var y = getRandomInt(-10, 10);
+    ctx.translate(x, y);
+    ctx.fillRect(300, 300, 10, 10);
 }
