ed63839be3a5f4c992f1d04a1f25b234996c9e4c
cudderson <codered1227@gmail.com>
2021-07-25 16:34:03 -0500

    begin pre sim animation fade outs

--- simulator.js
+++ simulator.js
@@ -455,7 +455,7 @@ async function runPreSimAnimations() {
     // flow:
     // (only with dialogue on!)
     await fadeInSimulationSettings();
-    // fade out sim settings
+    await fadeOutSimulationSettings();
     await fadeInSimulationIntro();
     // fade out intro
     await fadeInFakeGoal();
@@ -586,8 +586,8 @@ function fadeInSimulationSettings() {
 
                 ctx.fillStyle = `rgba(148, 0, 211, ${opacity})`;
                 ctx.font = "30px arial";
-                ctx.fillText("Simulation Settings", 300, 190);
-                ctx.fillRect(300, 195, 350, 1);
+                ctx.fillText("Simulation Settings", 300, 195);
+                ctx.fillRect(300, 197, 260, 1);
 
                 ctx.font = "24px arial";
                 ctx.fillText(`Initial Population:`, 300, 250);
@@ -622,6 +622,59 @@ function fadeInSimulationSettings() {
             }
         }
         start_settings_fadein = requestAnimationFrame(simSettingsFadeIn);
+    })
+}
+
+function fadeOutSimulationSettings() {
+    var finished = false;
+    var opacity = 1.00;
+    return new Promise(resolve => {
+        function simSettingsFadeOut() {
+            if (!finished) {
+                //animate
+                ctx.fillStyle = 'black';
+                ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+                ctx.fillStyle = `rgba(148, 0, 211, ${opacity})`;
+                ctx.font = "30px arial";
+                ctx.fillText("Simulation Settings", 300, 195);
+                ctx.fillRect(300, 197, 260, 1);
+
+                ctx.font = "24px arial";
+                ctx.fillText(`Initial Population:`, 300, 250);
+                ctx.fillText(`Gene Count:`, 300, 290);
+                ctx.fillText(`Movement Speed:`, 300, 330);
+                ctx.fillText(`Mutation Rate:`, 300, 370);
+                ctx.fillText(`Dialogue:`, 300, 410);
+                
+                ctx.fillStyle = `rgba(155, 245, 0, ${opacity})`;
+                ctx.fillText(`${TOTAL_ORGANISMS}`, 600, 250);
+                ctx.fillText(`${GENE_COUNT}`, 600, 290);
+                ctx.fillText(`${MAX_GENE}`, 600, 330);
+                ctx.fillText(`${MUTATION_RATE}`, 600, 370);
+
+                if (dialogue === false) {
+                    ctx.fillText(`Disabled`, 600, 410);
+                }
+                else {
+                    ctx.fillText(`Enabled`, 600, 410);
+                }
+
+
+                if (opacity <= 0.00) {
+                    finished = true;
+                }
+                else {
+                    opacity -= 0.02;
+                }
+                frame_id = requestAnimationFrame(simSettingsFadeOut);
+            }
+            else {
+                cancelAnimationFrame(frame_id);
+                resolve();
+            }
+        }
+        start_sim_settings_fadeout = requestAnimationFrame(simSettingsFadeOut);
     })
 }
 
